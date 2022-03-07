05e50f33e9dd138a567d869f71e2e6565fac1387
Tien Long Lam <lamtienlong9@gmail.com>
2022-03-07 23:29:06 +1100

    Comments

--- calc/Calculator.swift
+++ calc/Calculator.swift
@@ -1,32 +1,52 @@
 import Foundation
 
 class Calculator {
-    /// Perform Addition
-    ///
-    /// - Author: Jacktator
+    /// Performs addition of 2 numbers
     /// - Parameters:
-    ///   - no1: First number
-    ///   - no2: Second number
-    /// - Returns: The addition result
-    ///
-    /// - Warning: The result may yield Int overflow.
-    /// - SeeAlso: https://developer.apple.com/documentation/swift/int/2884663-addingreportingoverflow
+    ///   - no1: first number
+    ///   - no2: second number
+    /// - Returns: result of addition
     func add(no1: Int, no2: Int) -> Int {
         return no1 + no2
     }
+    /// Performs subtraction of 2 numbers
+    /// - Parameters:
+    ///   - no1: first number
+    ///   - no2: second number
+    /// - Returns: result of subtraction
     func subtract(no1: Int, no2: Int) -> Int {
         return no1 - no2
     }
+    /// Performs multiplication of 2 numbers
+    /// - Parameters:
+    ///   - no1: first number
+    ///   - no2: second number
+    /// - Returns: result of multiplication
     func multiply(no1: Int, no2: Int) -> Int {
         return no1 * no2
     }
+    /// Performs division of 2 numbers
+    /// - Parameters:
+    ///   - no1: first number to be divided
+    ///   - no2: second number that is divided by
+    /// - Returns: result of division
     func divide(no1: Int, no2: Int) -> Int {
         return no1 / no2
     }
+    /// Performs remainer operation of 2 numbers
+    /// - Parameters:
+    ///   - no1: first number to be divided
+    ///   - no2: second number that is divided by
+    /// - Returns: result of remainder operation
     func remain(no1: Int, no2: Int) -> Int {
         return no1 % no2
     }
     
+    /// Puts value into where the operator originally was and removes the 2 surrounding numbers
+    /// - Parameters:
+    ///   - value: value to be put in
+    ///   - array: array which the value will be put in
+    ///   - index: index of the operator where the value would be put in and the 2 surrounding numbers to be removed
     func putValueInArray(value: Int, array: inout [String], index:Int) {
         array[index] = String(value)
         array.remove(at: index+1)
@@ -36,16 +56,17 @@ class Calculator {
     
     func calculate(args: [String]) -> String {
         var items = args
-        var stageOne:Bool = false
-        var stageTwo:Bool = false
+        var phaseOne:Bool = false
+        var phaseTwo:Bool = false
         
         var tempResult:Int = 0
         var signIndex:Int = 1
         if(items.count > 1){
-            stageOne = true
+            phaseOne = true
         }
         
-        while stageOne {
+        // Phase 1 looks for and performs multiplication (x), division (/) and remainder(%) from left to right
+        while phaseOne {
             if(items.count == 1){
                 break
             }
@@ -72,7 +93,7 @@ class Calculator {
                 signIndex = 1
             }
             else if(signIndex == items.count-2) {
-                stageOne = false
+                phaseOne = false
             }
             else {
                 signIndex = signIndex + 2
@@ -82,9 +103,11 @@ class Calculator {
         tempResult = 0
         signIndex = 1
         if(items.count > 1){
-            stageTwo = true
+            phaseTwo = true
         }
-        while stageTwo {
+        
+        // Phase 2 looks for and performs addition (+) and subtraction (-) from left to right
+        while phaseTwo {
             if(items.count == 1){
                 break
             }
@@ -108,7 +131,7 @@ class Calculator {
                 signIndex = 1
             }
             else if(signIndex == items.count-2) {
-                stageTwo = false
+                phaseTwo = false
             }
             else {
                 signIndex = signIndex + 2
