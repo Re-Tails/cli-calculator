138bcdb0da73575713916a1f1b1be81b2dea0114
Tien Long Lam <lamtienlong9@gmail.com>
2022-03-07 22:43:55 +1100

    Implemented stage 2 and all remaining testing conditions

--- calc/Calculator.swift
+++ calc/Calculator.swift
@@ -3,7 +3,7 @@ import Foundation
 class Calculator {
     
     /// For multi-step calculation, it's helpful to persist existing result
-    var currentResult = 0;
+    var currentResult = 0
     
     /// Perform Addition
     ///
@@ -16,52 +16,86 @@ class Calculator {
     /// - Warning: The result may yield Int overflow.
     /// - SeeAlso: https://developer.apple.com/documentation/swift/int/2884663-addingreportingoverflow
     func add(no1: Int, no2: Int) -> Int {
-        return no1 + no2;
+        return no1 + no2
     }
     
     func calculate(args: [String]) -> String {
-        var items = ["12","x","3"];
-        var stageOne:Bool = false;
-        var stageTwo:Bool = false;
-        var tempResult:Int = 0;
-        var signIndex:Int = 1;
+        var items = args
+        var stageOne:Bool = false
+        var stageTwo:Bool = false
         
+        var tempResult:Int = 0
+        var signIndex:Int = 1
         if(items.count > 1){
-            stageOne = true;
+            stageOne = true
         }
             
         while stageOne {
             if(items.count == 1){
-                break;
+                break
             }
-            var itemsChanged:Bool = false;
+            var itemsChanged:Bool = false
             switch(items[signIndex]) {
             case "x":
                 tempResult = Int(items[signIndex-1])! * Int(items[signIndex+1])!
-                itemsChanged = true;
+                itemsChanged = true
             case "/":
                 tempResult = Int(items[signIndex-1])! / Int(items[signIndex+1])!
-                itemsChanged = true;
+                itemsChanged = true
             case "%":
                 tempResult = Int(items[signIndex-1])! % Int(items[signIndex+1])!
-                itemsChanged = true;
+                itemsChanged = true
             default:
-                continue
+                ()
             }
             if(itemsChanged) {
-                items[signIndex] = String(tempResult);
-                items.remove(at: signIndex+1);
-                items.remove(at: signIndex-1);
-                signIndex = 1;
+                items[signIndex] = String(tempResult)
+                items.remove(at: signIndex+1)
+                items.remove(at: signIndex-1)
+                signIndex = 1
             }
             else if(signIndex == items.count-2) {
-                stageOne = false;
+                stageOne = false
             }
             else {
-                signIndex = signIndex + 2;
+                signIndex = signIndex + 2
             }
         }
-        let result = String(items[0]);
+        
+        tempResult = 0
+        signIndex = 1
+        if(items.count > 1){
+            stageTwo = true
+        }
+        while stageTwo {
+            if(items.count == 1){
+                break
+            }
+            var itemsChanged:Bool = false
+            switch(items[signIndex]) {
+            case "+":
+                tempResult = Int(items[signIndex-1])! + Int(items[signIndex+1])!
+                itemsChanged = true
+            case "-":
+                tempResult = Int(items[signIndex-1])! - Int(items[signIndex+1])!
+                itemsChanged = true
+            default:
+                ()
+            }
+            if(itemsChanged) {
+                items[signIndex] = String(tempResult)
+                items.remove(at: signIndex+1)
+                items.remove(at: signIndex-1)
+                signIndex = 1
+            }
+            else if(signIndex == items.count-2) {
+                stageTwo = false
+            }
+            else {
+                signIndex = signIndex + 2
+            }
+        }
+        let result = String(Int(items[0])!)
         return(result)
     }
 }
--- calc/main.swift
+++ calc/main.swift
@@ -3,10 +3,35 @@ import Foundation
 var args = ProcessInfo.processInfo.arguments
 args.removeFirst() // remove the name of the program
 
+
+var validArgs:Bool = true
+// Check arguments count is odd
+if args.count % 2 != 1 {
+    validArgs = false
+}
+// Check numbers are valid
+for i in stride(from: 0, to: args.count, by: 2){
+    guard let _ = Int(args[i]) else{
+        validArgs = false
+        break
+    }
+}
+// Check signs are valid
+for i in stride(from: 1, to: args.count, by: 2){
+    guard ["+", "-", "x", "/", "%"].contains(args[i]) else{
+        validArgs = false
+        break
+    }
+}
+
+
+guard validArgs else {
+    exit(1)
+}
+
 // Initialize a Calculator object
-let calculator = Calculator();
-
-var result = calculator.calculate(args: args);
+let calculator = Calculator()
 // Calculate the result
-
+var result = calculator.calculate(args: args)
 print(result)
+exit(0)
