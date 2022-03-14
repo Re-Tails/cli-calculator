52e2218b99037da4718a4e0f9193edc9456466e0
Tien Long Lam <lamtienlong9@gmail.com>
2022-03-14 14:12:37 +1100

    Added comments and rename some vars

--- calc/Calculator.swift
+++ calc/Calculator.swift
@@ -54,6 +54,9 @@ class Calculator {
         
     }
     
+    /// Performs algebraic calculations on the expression inputted and outputs the result
+    /// - Parameter args: the array contains the expression, each item is either a number or operator
+    /// - Returns: the result of the inputted expression
     func calculate(args: [String]) -> String {
         var items = args
         var phaseOne:Bool = false
@@ -62,40 +65,46 @@ class Calculator {
         var tempResult:Int = 0
         var signIndex:Int = 1
         if(items.count > 1){
+            // checks to ensure there is more than 1 number/item in the inputted expression
+            // if true, start the calculation process with multiplication (x), division (/) and remainder(%) from left to right
             phaseOne = true
         }
         
         // Phase 1 looks for and performs multiplication (x), division (/) and remainder(%) from left to right
         while phaseOne {
             if(items.count == 1){
+                // if there is only 1 item left, the calculation is finished so break from the loop
                 break
             }
             
-            let no1:Int = Int(items[signIndex-1])!
-            let no2:Int = Int(items[signIndex+1])!
+            let leftNum:Int = Int(items[signIndex-1])! //the number to the left of the sign/operator
+            let rightNum:Int = Int(items[signIndex+1])! //the number to the right of the sign/operator
             var itemsChanged:Bool = false
             
             switch(items[signIndex]) {
             case "x":
-                tempResult = multiply(no1: no1, no2: no2)
+                tempResult = multiply(no1: leftNum, no2: rightNum)
                 itemsChanged = true
             case "/":
-                tempResult = divide(no1: no1, no2: no2)
+                tempResult = divide(no1: leftNum, no2: rightNum)
                 itemsChanged = true
             case "%":
-                tempResult = remain(no1: no1, no2: no2)
+                tempResult = remain(no1: leftNum, no2: rightNum)
                 itemsChanged = true
             default:
                 ()
             }
             if(itemsChanged) {
+                // replace the sign with the result of that operator and the 2 numbers surrounding it
                 putValueInArray(value: tempResult, array: &items, index: signIndex)
                 signIndex = 1
             }
             else if(signIndex == items.count-2) {
+                // phase 1 calculation ends if reaches the end of the expression with no items changed or operations/calculations handled
                 phaseOne = false
             }
             else {
+                // continue phase 1 with the next sign if not at the end of the expression and no calculations yet
                 signIndex = signIndex + 2
             }
         }
@@ -103,41 +112,48 @@ class Calculator {
         tempResult = 0
         signIndex = 1
         if(items.count > 1){
+            // checks to ensure there is more than 1 number/item in the inputted expression
+            // if true, continue the calculation process with addition (+) and subtraction (-) from left to right
             phaseTwo = true
         }
         
         // Phase 2 looks for and performs addition (+) and subtraction (-) from left to right
         while phaseTwo {
             if(items.count == 1){
+                // if there is only 1 item left, the calculation is finished so break from the loop
                 break
             }
             
-            let no1:Int = Int(items[signIndex-1])!
-            let no2:Int = Int(items[signIndex+1])!
+            let leftNum:Int = Int(items[signIndex-1])! //the number to the left of the sign/operator
+            let rightNum:Int = Int(items[signIndex+1])! //the number to the right of the sign/operator
             var itemsChanged:Bool = false
             
             switch(items[signIndex]) {
             case "+":
-                tempResult = add(no1: no1, no2: no2)
+                tempResult = add(no1: leftNum, no2: rightNum)
                 itemsChanged = true
             case "-":
-                tempResult = subtract(no1: no1, no2: no2)
+                tempResult = subtract(no1: leftNum, no2: rightNum)
                 itemsChanged = true
             default:
                 ()
             }
             if(itemsChanged) {
+                // replace the sign with the result of that operator and the 2 numbers surrounding it
                 putValueInArray(value: tempResult, array: &items, index: signIndex)
                 signIndex = 1
             }
             else if(signIndex == items.count-2) {
+                // phase 2 calculation ends if reaches the end of the expression with no items changed or operations/calculations handled
                 phaseTwo = false
             }
             else {
+                // continue phase 2 with the next sign if not at the end of the expression and no calculations yet
                 signIndex = signIndex + 2
             }
         }
         let result = String(Int(items[0])!)
+        // the result will be the first item of the array as there will only be 1 item/number left
         return(result)
     }
 }
